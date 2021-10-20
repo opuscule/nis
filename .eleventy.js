bdb97cce5ed76787b1853c2c3335a5f75d280cf5
Robert C. Bowman III <bowman@opuscule.com>
2021-10-20 06:23:26 -0400

    Styles Asset Path

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function(eleventyConfig) {
-	eleventyConfig.addPassthroughCopy('/src/css');
+	eleventyConfig.addPassthroughCopy('/src/css/styles.css');
 	eleventyConfig.addPassthroughCopy('/src/images');
 	eleventyConfig.addPassthroughCopy('/src/js');
 	eleventyConfig.addPassthroughCopy('/src/_redirects');
