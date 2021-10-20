304f8771161e480a08f0c63d43ed3c91870c325e
Robert C. Bowman III <bowman@opuscule.com>
2021-10-20 06:30:37 -0400

    Passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('/src/css/styles.css');
 	eleventyConfig.addPassthroughCopy('/src/images');
-	eleventyConfig.addPassthroughCopy('/src/js');
+	eleventyConfig.addPassthroughCopy('./src/js');
 	eleventyConfig.addPassthroughCopy('/src/_redirects');
 	return {
 		dir: {
