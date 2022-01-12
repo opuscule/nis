45ae6d3755d1e223d889327892d69a7e47177d13
Robert C. Bowman III <bowman@opuscule.com>
2022-01-12 11:32:51 -0500

    Careers

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('./src/images');
 	eleventyConfig.addPassthroughCopy('./src/js');
 	eleventyConfig.addPassthroughCopy('./src/styles');
+	eleventyConfig.addPassthroughCopy('./src/resource');
 	eleventyConfig.addPassthroughCopy('./src/_redirects');
 	return {
 		dir: {
