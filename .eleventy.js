86069a42216a9fc6ac46ecfec36c85bb9d51fc16
Robert C. Bowman III <bowman@opuscule.com>
2022-04-26 06:20:00 -0400

    Update Bios, Minify

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,12 @@
+const eleventyPluginFilesMinifier = require("@sherby/eleventy-plugin-files-minifier");
+
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('./src/images');
 	eleventyConfig.addPassthroughCopy('./src/js');
 	eleventyConfig.addPassthroughCopy('./src/styles');
 	eleventyConfig.addPassthroughCopy('./src/resource');
 	eleventyConfig.addPassthroughCopy('./src/_redirects');
+	eleventyConfig.addPlugin(eleventyPluginFilesMinifier);
 	return {
 		dir: {
 			input:"src",
